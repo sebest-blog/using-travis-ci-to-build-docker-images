4e62e41d145caafcd2f76ed42315f0055a7e7d13
Sebastien Estienne <sebastien.estienne@gmail.com>
2015-10-30 20:56:19 +0800

    Demo app is a small http server

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM scratch
 ADD main /
+EXPOSE 8000
 CMD ["/main"]
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Request %s (go)\n", r.URL.Path)
+}
 
 func main() {
-	fmt.Printf("Hello world!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", nil)
 }
